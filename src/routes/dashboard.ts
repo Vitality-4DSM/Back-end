deb6449e77fd14efdce6c3cb9d7db0a475246b1c
Jonatas Mathias <jonatasmathiasdallo@gmail.com>
2023-11-13 10:45:39 -0300

    Adição de api key e banco relacional online

--- src/routes/dashboard.ts
+++ src/routes/dashboard.ts
@@ -1,11 +1,12 @@
 import { Router, Request, Response } from "express";
 import { authorization} from "../autenticacao";
 import dashboard from '../controllers/dashboard';
+import { apiKey } from "../middlewares";
 
 const routes = Router();
 
-routes.get('/:id', dashboard.getAll);
-routes.get('/nometabela/:id', dashboard.getNometabela);
+routes.get('/:id', apiKey, dashboard.getAll);
+routes.get('/nometabela/:id', apiKey, dashboard.getNometabela);
 
 routes.use((_: Request, res: Response) => res.json({ error: "Requisição desconhecida" }));
 export default routes;
--- src/routes/tipo_parametro.ts
+++ src/routes/tipo_parametro.ts
@@ -1,6 +1,7 @@
 import { Router, Request, Response } from "express";
 import TipoParametro from '../controllers/tipo_parametro';
 import { authorization } from "../autenticacao";
+import { apiKey } from "../middlewares";
 
 const routes = Router();
 // routes.post('/', TipoParametro.create);
@@ -10,11 +11,11 @@ const routes = Router();
 // routes.delete('/:id', TipoParametro.delete);
 
 // SOMENTE O ADMIN ACESSA CREATE E O DELETE
-routes.post('/', authorization, TipoParametro.create);
-routes.get('/:id', TipoParametro.getById);
-routes.get('/',  TipoParametro.getAll);
-routes.put('/', authorization, TipoParametro.update);
-routes.delete('/:id', authorization,  TipoParametro.delete);
+routes.post('/', authorization, apiKey, TipoParametro.create);
+routes.get('/:id', apiKey, TipoParametro.getById);
+routes.get('/', apiKey,  TipoParametro.getAll);
+routes.put('/', apiKey, authorization, TipoParametro.update);
+routes.delete('/:id', apiKey, authorization,  TipoParametro.delete);
 
 
 routes.use((_: Request, res: Response) => res.json({ error: "Requisição desconhecida" }));
