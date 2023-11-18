230da7553864577c73a60865ef39e161aee0c809
Jonatas Mathias <jonatasmathiasdallo@gmail.com>
2023-11-17 22:43:27 -0300

    Alteração no banco, para poder rodar o deploy da main

--- src/config/bd_config.ts
+++ src/config/bd_config.ts
@@ -1,9 +1,11 @@
-import { Sequelize } from "sequelize";
+import { Sequelize } from 'sequelize';
 
-const db = new Sequelize('api', 'root', 'root', {
-    host: 'localhost',
-    dialect: 'mysql',
-    logging: true
-});
+// const db = new Sequelize('api', 'root', 'root', {
+//     host: 'localhost',
+//     dialect: 'mysql',
+//     logging: true
+// });
+
+const db = new Sequelize('postgresql://Jonatas-Dallo:Y7QiL3dgwPeJ@ep-rapid-union-63652562.us-east-2.aws.neon.tech/vitality-gaia?sslmode=require');
 
 export default db;
